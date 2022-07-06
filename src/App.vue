00226569e5c21f7fa0164e6b3880f44af75652ec
franciowel <spina_francesco@blu.it>
2022-07-06 16:05:01 +0200

    inizio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    
+    <header>
+        <myHeaderDC/>
+    </header>
+
+    <!-- MAIN -->
+
+    <!-- FOOTER -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myHeaderDC from './components/myHeaderDC.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myHeaderDC,
   }
 }
 </script>
